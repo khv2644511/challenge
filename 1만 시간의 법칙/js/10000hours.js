335aaea91d77f49b0f9884251067e81bdf0b589c
khv2644511 <khv2644511@naver.com>
2022-09-12 22:51:51 +0900

    과제

---
+++ 1만 시간의 법칙/js/10000hours.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector('.cont-modal');
+const btnOpenPopup = document.querySelector('.btn-go-final');
+
+btnOpenPopup.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
